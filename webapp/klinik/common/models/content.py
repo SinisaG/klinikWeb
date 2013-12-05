f6ad17a039ce6ad587a4686865663795733f4d74
Sinisa <sinisa@hackandcraft.com>
2013-12-05 11:57:27 +0100

    content linked with db,wideo working on new devices

--- webapp/klinik/common/models/content.py
+++ webapp/klinik/common/models/content.py
@@ -1,6 +1,6 @@
 from operator import itemgetter
 import os
-from hnc.apiclient import Mapping, TextField
+from hnc.apiclient import Mapping, TextField, IntegerField, DictField, ListField
 from hnc.apiclient.backend import ClientTokenProc
 from hnc.apps.static_content.views import ContentModel
 from pyramid.path import AssetResolver
@@ -30,16 +30,23 @@ class HST(Mapping):
 
     @property
     def template(self):
-        return website_t("home_blocks/{}.html".format(self.name))
+        return website_t("{}.html".format(self.name))
 
 
+class Image(Mapping):
+    id = IntegerField()
+    url = TextField()
 
+class Text (Mapping):
+    id = IntegerField()
+    title = TextField()
+    body = TextField()
 
 HOMEPAGE_BLOCKS = [HST(**f) for f in sorted(TEMPLATES, key = itemgetter('name'))]
-
-
 
 
 GetStaticRawContentProc  = ClientTokenProc("/web/content")
 GetStaticContentProc  = ClientTokenProc("/web/content", root_key="Content", result_cls=ContentModel)
 SetStaticContentProc = ClientTokenProc("/web/content/set")
+GetImagesProc = ClientTokenProc("/web/image/all", root_key="Images", result_cls=Image, result_list=True)
+GetTextsProc = ClientTokenProc("/web/text/all", root_key="Texts", result_cls=Text, result_list=True)
--- webapp/klinik/views/website/index.py
+++ webapp/klinik/views/website/index.py
@@ -1,10 +1,16 @@
 from operator import attrgetter
 from hnc.forms.handlers import FormHandler
+from klinik.common.models.content import GetImagesProc, GetTextsProc
 from klinik.views.website.forms import ContactForm
 
 
 class index(FormHandler):
     form =ContactForm
+
+    def add_globals(self, request, result):
+        result['images'] = GetImagesProc(request)
+        result['texts'] = GetTextsProc(request)
+        return result
 
 
 def empty(context, request):
